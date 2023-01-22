00de3ab5773c0f30c5e7f687dacc8debdf3b1cc2
Rich <piemonkey@protonmail.ch>
2023-01-22 19:15:06 +0100

    Add existing signup info to project staffing display chart

--- client/components/common/ProjectStaffingDisplay.jsx
+++ client/components/common/ProjectStaffingDisplay.jsx
@@ -14,20 +14,34 @@ const chartOptions = {
   },
 }
 
-const processData = (staffing) => {
-  const wanted = staffing.wanted.map((want, i) => Math.max(0, want - staffing.needed[i]))
+const processData = (staffing, signup) => {
+  // Maybe we could do this in the method to avoid having this logic split
+  let signedUp = []
+  if (signup) {
+    const signupStart = moment(signup.start)
+    const signupEnd = moment(signup.end)
+    signedUp = staffing.days.map(day =>
+      (moment(day).isBetween(signupStart, signupEnd, 'days', '[]') ? 1 : 0))
+  }
+  const confirmed = staffing.confirmed.map((num, i) => num - (signedUp[i] ?? 0))
+  // FIXME i18n!
   return {
     labels: staffing.days.map((t) => moment(t).format('MMM Do')),
     datasets: [
-      { label: 'confirmed', data: staffing.confirmed, backgroundColor: '#D6E9C6' },
+      ...!signup ? [] : [{
+        label: signup.status,
+        data: signedUp,
+        backgroundColor: '#3944E8',
+      }],
+      { label: 'filled', data: confirmed, backgroundColor: '#D6E9C6' },
       { label: 'needed', data: staffing.needed, backgroundColor: '#CD5C5C' },
-      { label: 'wanted', data: wanted, backgroundColor: '#ffe94D' },
+      { label: 'wanted', data: staffing.wanted, backgroundColor: '#ffe94D' },
     ],
   }
 }
 
-export const ProjectStaffingDisplay = ({ staffing }) => {
-  const barData = useMemo(() => processData(staffing), [staffing])
+export const ProjectStaffingDisplay = ({ staffing, signup }) => {
+  const barData = useMemo(() => processData(staffing, signup), [staffing, signup])
   return (
     <div className="chart-container">
       <Bar data={barData} options={chartOptions} />
