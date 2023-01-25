f598a049258869867e51dab86cbdc6afcb782dc3
Rich <piemonkey@protonmail.ch>
2023-01-25 12:42:07 +0100

    Support initialising an instance with the same name twice
    Without a server restart, Meteor would crash on attempting to create
    collections. Instead it now just re-uses collection if it already
    created them, so we can e.g. create a new instance for event migration

--- both/collections/initCollections.js
+++ both/collections/initCollections.js
@@ -10,6 +10,19 @@ import { initCollectionUtils } from './utils'
 checkNpmVersions({ 'simpl-schema': '1.x' }, 'goingnowhere:volunteers')
 SimpleSchema.extendOptions(['autoform'])
 
+// Unfortunately Meteor doesn't like us initialising collections twice but doesn't
+// give us a way to clean them up, so incase we create an instance with the same eventName
+// twice without restarting, keep a singleton map and reuse.
+const allCollections = new Map()
+const createCollection = (name) => {
+  let collection = allCollections.get(name)
+  if (!collection) {
+    collection = new Mongo.Collection(name)
+    allCollections.set(name, collection)
+  }
+  return collection
+}
+
 export const initCollections = (eventName) => {
   const collections = {}
   const prefix = `${eventName}.Volunteers`
@@ -26,23 +39,23 @@ export const initCollections = (eventName) => {
   }
 
   // Org
-  collections.team = new Mongo.Collection(`${prefix}.team`)
+  collections.team = createCollection(`${prefix}.team`)
   collections.team.attachSchema(schemas.team)
 
-  collections.department = new Mongo.Collection(`${prefix}.department`)
+  collections.department = createCollection(`${prefix}.department`)
   collections.department.attachSchema(schemas.department)
 
-  collections.division = new Mongo.Collection(`${prefix}.division`)
+  collections.division = createCollection(`${prefix}.division`)
   collections.division.attachSchema(schemas.division)
 
   // duties
-  collections.task = new Mongo.Collection(`${prefix}.teamTasks`)
+  collections.task = createCollection(`${prefix}.teamTasks`)
   collections.task.attachSchema(schemas.task)
   if (Meteor.isServer) {
     collections.task.createIndex({ parentId: 1 })
   }
 
-  collections.shift = new Mongo.Collection(`${prefix}.teamShifts`)
+  collections.shift = createCollection(`${prefix}.teamShifts`)
   collections.shift.attachSchema(schemas.shift)
   if (Meteor.isServer) {
     collections.shift.createIndex({ parentId: 1 })
@@ -53,36 +66,36 @@ export const initCollections = (eventName) => {
   if (Meteor.isClient) {
     // Create client-side only collection to reactively aggregate into
     // If preformance is a problem we could aggregate into a collection inside Mongo
-    collections.shiftGroups = new Mongo.Collection(`${prefix}.shiftGroups`)
+    collections.shiftGroups = createCollection(`${prefix}.shiftGroups`)
   }
 
-  collections.project = new Mongo.Collection(`${prefix}.projects`)
+  collections.project = createCollection(`${prefix}.projects`)
   collections.project.attachSchema(schemas.project)
   if (Meteor.isServer) {
     collections.project.createIndex({ parentId: 1 })
   }
 
-  collections.lead = new Mongo.Collection(`${prefix}.lead`)
+  collections.lead = createCollection(`${prefix}.lead`)
   collections.lead.attachSchema(schemas.lead)
   if (Meteor.isServer) {
     collections.lead.createIndex({ parentId: 1 })
   }
 
   // Data for the volunteer Form
-  collections.volunteerForm = new Mongo.Collection(`${prefix}.volunteerForm`)
+  collections.volunteerForm = createCollection(`${prefix}.volunteerForm`)
   if (Meteor.isServer) {
     collections.volunteerForm.createIndex({ userId: 1 })
   }
 
   // User duties
-  collections.signups = new Mongo.Collection(`${prefix}.signups`)
+  collections.signups = createCollection(`${prefix}.signups`)
   collections.signups.attachSchema(schemas.signup)
   if (Meteor.isServer) {
     // we enforce using a unique index that a person cannot sign up twice for the same duty
     collections.signups.createIndex({ userId: 1, shiftId: 1 })
   }
 
-  collections.rotas = new Mongo.Collection(`${prefix}.rotas`)
+  collections.rotas = createCollection(`${prefix}.rotas`)
   collections.rotas.attachSchema(schemas.rota)
   if (Meteor.isServer) {
     collections.rotas.createIndex({ parentId: 1 })
