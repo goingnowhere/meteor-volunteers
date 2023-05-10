f503ce1867a0be291d9e20aeb5f1b07682c910c2
Rich <piemonkey@protonmail.ch>
2023-05-10 13:13:37 +0200

    Fix project staffing display for pending shifts

--- client/components/common/ProjectStaffingDisplay.jsx
+++ client/components/common/ProjectStaffingDisplay.jsx
@@ -17,13 +17,33 @@ const chartOptions = {
 const processData = (staffing, signup) => {
   // Maybe we could do this in the method to avoid having this logic split
   let signedUp = []
+  let { confirmed, needed, wanted } = staffing
   if (signup) {
     const signupStart = moment(signup.start)
     const signupEnd = moment(signup.end)
     signedUp = staffing.days.map(day =>
       (moment(day).isBetween(signupStart, signupEnd, 'days', '[]') ? 1 : 0))
+    if (signup.status === 'confirmed') {
+      confirmed = staffing.confirmed.map((num, i) => num - (signedUp[i] ?? 0))
+    } else if (signup.status === 'pending') {
+      const newNeeded = []
+      const newWanted = []
+      signedUp.forEach((num, i) => {
+        newWanted[i] = wanted[i]
+        if (!signedUp[i]) {
+          newNeeded[i] = needed[i]
+        } else {
+          newNeeded[i] = needed[i] - signedUp[i]
+          if (newNeeded[i] < 0) {
+            newWanted[i] = wanted[i] + newNeeded[i] // + because newConfirmed is negative
+            newNeeded[i] = 0
+          }
+        }
+      })
+      needed = newNeeded
+      wanted = newWanted
+    }
   }
-  const confirmed = staffing.confirmed.map((num, i) => num - (signedUp[i] ?? 0))
   // FIXME i18n!
   return {
     labels: staffing.days.map((t) => moment(t).format('MMM Do')),
@@ -34,8 +54,8 @@ const processData = (staffing, signup) => {
         backgroundColor: '#3944E8',
       }],
       { label: 'filled', data: confirmed, backgroundColor: '#D6E9C6' },
-      { label: 'needed', data: staffing.needed, backgroundColor: '#CD5C5C' },
-      { label: 'wanted', data: staffing.wanted, backgroundColor: '#ffe94D' },
+      { label: 'needed', data: needed, backgroundColor: '#CD5C5C' },
+      { label: 'wanted', data: wanted, backgroundColor: '#ffe94D' },
     ],
   }
 }
